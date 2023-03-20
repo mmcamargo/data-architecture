0d26db18d90101c5306a70d5a2f0ea300e3da425
MMCamargo <matheusmarianodecamargo@gmail.com>
2023-03-19 23:24:46 -0300

    #

--- src/app/features/tasks/validators/update-task.validator.ts
+++ src/app/features/tasks/validators/update-task.validator.ts
@@ -1,9 +1,4 @@
-import {
-	HttpHelper,
-	DefaultMessagesHelper,
-} from '../../../shared/utils/helpers';
-import { UpdateTaskUseCase } from '../usecases';
-import { TasksRepository } from '../repositories';
+import { HttpHelper } from '../../../shared/utils/helpers';
 import { Request, Response, NextFunction } from 'express';
 
 export const updateTaskValidator = async (
@@ -11,19 +6,10 @@ export const updateTaskValidator = async (
 	res: Response,
 	next: NextFunction
 ) => {
-	const { uid } = req.params;
 	const { isArchived, title, content } = req.body;
 
 	if (!isArchived && !title && !content) {
 		return HttpHelper.badRequest(res, 'Nenhum valor encontrado.', 404);
-	}
-
-	const useCase = new UpdateTaskUseCase(new TasksRepository());
-
-	const isTaskRegistered = await useCase.checkTaskUid(uid);
-
-	if (!isTaskRegistered) {
-		return HttpHelper.badRequest(res, 'Tarefa não encontrada.', 404);
 	}
 
 	next();
