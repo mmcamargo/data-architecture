1ac18b1bc85105c3e2d1ccbd4e6ea77270855711
MMCamargo <matheusmarianodecamargo@gmail.com>
2023-03-25 18:46:04 -0300

    feat: redis configs

---
+++ src/main/database/redis.connection.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import Redis from 'ioredis';
+
+export class RedisConnection {
+	private static _connection: Redis;
+
+	public static async connect(): Promise<void> {
+		if (!this._connection) {
+			this._connection = new Redis({
+				host: process.env.REDIS_HOST,
+				port: Number(process.env.REDIS_PORT),
+				username: process.env.REDIS_USERNAME,
+				password: process.env.REDIS_PASSWORD,
+			});
+		}
+	}
+
+	public static get connection() {
+		if (!this._connection) {
+			throw new Error('CACHE_CONNECTION_ERROR');
+		}
+
+		return this._connection;
+	}
+}
